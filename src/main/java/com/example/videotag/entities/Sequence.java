855803418567bf29d13b39ced451418436bf6c82
Tom <tom-vautray@hotmail.fr>
2021-10-04 16:19:20 +0200

    Completed class Sequence

--- src/main/java/com/example/videotag/entities/Sequence.java
+++ src/main/java/com/example/videotag/entities/Sequence.java
@@ -2,7 +2,31 @@ package com.example.videotag.entities;
 
 import java.util.List;
 
-public class Sequence {
+import com.example.videotag.domain.VideoTagDomainClass;
+
+public class Sequence extends VideoTagDomainClass {
 	private Video video;
 	private List<AbstractTag> tagList;
+	
+	public Sequence(Video video, List<AbstractTag> tagList) {
+		super();
+		setVideo(video);
+		setTagList(tagList);
+	}
+	
+	public Video getVideo() {
+		return video;
+	}
+	
+	private void setVideo(Video video) {
+		this.video = video;
+	}
+	
+	public List<AbstractTag> getTagList() {
+		return tagList;
+	}
+	
+	private void setTagList(List<AbstractTag> tagList) {
+		this.tagList = tagList;
+	}
 }
